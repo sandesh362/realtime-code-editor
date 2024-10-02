9aed9554e6a77accacfd9d603dd4a5fed6b6b16f
Omkar <sonawaneomkar2306@gmail.com>
2024-10-02 20:49:03 +0530

    Completed

--- src/pages/EditorPage.js
+++ src/pages/EditorPage.js
@@ -131,11 +131,6 @@ const EditorPage = () => {
             <div className="aside">
                 <div className="asideInner">
                     <div className="logo">
-                        <img
-                            className="logoImage"
-                            src="/code-sync.png"
-                            alt="logo"
-                        />
                     </div>
                     <h3>Connected</h3>
                     <div className="clientsList">
